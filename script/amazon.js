c9fb6cd6639c74a4c905fe5c7de48967721bb767
Honoredcodes <honoredcodes@gmail.com>
2024-12-18 16:58:50 +0200

    Made some changes

--- script/amazon.js
+++ script/amazon.js
@@ -1,40 +1,8 @@
-const products = [
-    {
-        image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-        name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-        rating :{
-            stars: 4.5,
-            count: 87
-        },
-        priceCents: 1090
-    },
-    {
-        image: "images/products/intermediate-composite-basketball.jpg",
-        name: "Intermediate Size Basketball",
-        rating :{
-            stars: 4,
-            count: 127
-        },
-        priceCents: 2095
-    },
-    {
-        image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-        name: "Adults Plain Cotton T-Shirt - 2 Pack",
-        rating :{
-            stars: 4.5,
-            count: 56
-        },
-        priceCents: 799
-    }
-
-];
-
 let productsHTML = "";
 let productContainer = document.querySelector(".products-grid");
 
-
-products.forEach((product)=>{
-    productsHTML += `
+products.forEach((product) => {
+  productsHTML += `
     <div class="product-container">
         <div class="product-image-container">
           <img class="product-image" src="${product.image}">
@@ -43,13 +11,15 @@ products.forEach((product)=>{
         ${product.name}
         </div>
         <div class="product-rating-container">
-          <img class="product-rating-stars" src="images/ratings/rating-${(product.rating.stars*10)}.png">
+          <img class="product-rating-stars" src="images/ratings/rating-${
+            product.rating.stars * 10
+          }.png">
           <div class="product-rating-count link-primary">
             ${product.rating.count}
           </div>
         </div>
         <div class="product-price">
-          $${(product.priceCents/100).toFixed(2)}
+          $${(product.priceCents / 100).toFixed(2)}
         </div>
 
         <div class="product-quantity-container">
@@ -71,12 +41,38 @@ products.forEach((product)=>{
           <img src="images/icons/checkmark.png">
           Added
         </div>
-        <button class="add-to-cart-button button-primary">
+        <button class="add-to-cart-button button-primary" data-product-Id = "${
+          product.id
+        }">
           Add to Cart
         </button>
-    </div>`
+    </div>`;
+});
 
-   
+productContainer.innerHTML = productsHTML;
 
-})
- productContainer.innerHTML = productsHTML;
+const addToCartBtn = document.querySelectorAll(".add-to-cart-button");
+
+addToCartBtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    const productId = button.dataset.productId;
+
+    let matchingItem;
+
+    cart.forEach((item) => {
+      if (item.productId === productId) {
+        matchingItem = item;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity++;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1,
+      });
+    }
+    console.log(cart);
+  });
+});
